4dcbd55f205a62399394f6015cf90b82a719880d
HeiKayYoung <kyoungmihaha@gmail.com>
2023-10-25 13:18:56 +0900

    create And Update

---
+++ src/main/java/com/tech/bikemember/controller/BikeMemberController.java
@@ -0,0 +1,57 @@
+package com.tech.bikemember.controller;
+
+import com.tech.bikemember.exception.ResourceNotFoundException;
+import com.tech.bikemember.model.BikeMember;
+import com.tech.bikemember.repository.BikeMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/bikemembers")
+public class BikeMemberController {
+
+    @Autowired
+    private BikeMemberRepository bikeMemberRepository;
+    //DB와 연결시켜서 사용하겠다는 의미
+
+    @GetMapping
+    public List<BikeMember> getAllBikeMembers(){
+        System.out.println("List<BikeMember> getAllBikeMembers");
+        return bikeMemberRepository.findAll();
+    }
+    @PostMapping
+    public BikeMember createBikeMember(@RequestBody BikeMember bikeMember){
+//        Object BikeMember 로 받으면 bikeMember 이름으로 쓰겠다
+//        Post방식으로 보낸것이 알아서 PostMapping 찾아서
+        System.out.println("BikeMember createBikeMember db save");
+
+        return bikeMemberRepository.save(bikeMember);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<BikeMember> getBikeMemberById(@PathVariable long id){
+        System.out.println("ResponseEntity<BikeMember> getBikeMemberById");
+        BikeMember bikeMember=bikeMemberRepository.findById(id).
+                orElseThrow(()->new ResourceNotFoundException("BikeMember not exist with id:"+id));
+
+        return ResponseEntity.ok(bikeMember);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<BikeMember> updateBikeMember(@PathVariable long id,
+                                                       @RequestBody BikeMember bikeMember){
+        System.out.println("ResponseEntity<BikeMember> updateBikeMember");
+        BikeMember updateBikeMember=bikeMemberRepository.findById(id).
+                orElseThrow(()->new ResourceNotFoundException("BikeMember not exist with id:"+id));
+        updateBikeMember.setFirstName(bikeMember.getFirstName());
+        updateBikeMember.setLastName(bikeMember.getLastName());
+        updateBikeMember.setEmailId(bikeMember.getEmailId());
+        bikeMemberRepository.save(updateBikeMember);
+
+        return ResponseEntity.ok(updateBikeMember);
+    }
+}
